c2abd9705b6373dd37476331b1edc516e3bbbed1
dzejkk <kontrajakub2@gmail.com>
2025-05-21 13:27:45 +0200

    postup na dalsiu kapitolu

---
+++ Instaluj si, nastavim ti/app.js
@@ -0,0 +1,28 @@
+// let a = parseInt(prompt("zadaj prve cislo"));
+// let b = parseInt(prompt("zadaj druhe cislo"));
+
+// function sum(a, b) {
+//   console.log(a + b);
+//   return a + b;
+// }
+
+// sum(a, b);
+
+//
+
+let pocetProduktov = 5;
+let cenaProduktu = 600;
+
+function how_much(quantity, price) {
+  const app = document.getElementById("app");
+
+  const display = document.createElement("p");
+
+  display.textContent = `kupil si ${quantity} predmetov za ${
+    price * quantity
+  } dokopy`;
+
+  app.appendChild(display);
+}
+
+how_much(5, 1500);
