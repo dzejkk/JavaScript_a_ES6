e697bce862ddaa654357b8811755fb2902880610
dzejkk <kontrajakub2@gmail.com>
2025-08-13 11:43:58 +0200

    added functions

--- SVG (a strict mode)/app.js
+++ SVG (a strict mode)/app.js
@@ -6,7 +6,6 @@ let progress = document.querySelector(".progress-warn");
 let textarea = document.querySelector("textarea");
 let pathLenght = progress.getAttribute("r") * 2 * Math.PI; // get perimeter
 const counter = document.querySelector(".counter");
-
 let tweetLength = 70;
 const warningZone = Math.floor(tweetLength * (1 / 2));
 const dangerZone = Math.floor(tweetLength * (4 / 5));
@@ -16,21 +15,33 @@ progress.style.strokeDashoffset = pathLenght + "px";
 
 textarea.addEventListener("input", (e) => {
   let stringLength = textarea.value.length;
+  handleProgress(stringLength);
+  handleColors(stringLength);
+  handleCounter(stringLength);
+});
 
+// counter.textContent = tweetLength - stringLength;
+// counter.classList.toggle("danger", stringLength >= tweetLength);
+
+// progress.classList.toggle("danger", stringLength > dangerZone);
+// progress.classList.toggle("warning", stringLength >= warningZone);
+// progress.classList.toggle("tragedy", stringLength >= tweetLength);
+
+function handleProgress(stringLength) {
   if (stringLength <= tweetLength) {
     let tweetLenghtRatio = stringLength / tweetLength; // to get ratio between full lenght and remaining
     let newOffset = pathLenght - pathLenght * tweetLenghtRatio + "px";
 
     progress.style.strokeDashoffset = newOffset;
-
-    // handle counter
-
-    // handle color
   }
+}
 
-  counter.textContent = tweetLength - stringLength;
-  counter.classList.toggle("danger", stringLength >= tweetLength);
-
+function handleColors(stringLength) {
   progress.classList.toggle("danger", stringLength > dangerZone);
   progress.classList.toggle("warning", stringLength >= warningZone);
-});
+  progress.classList.toggle("tragedy", stringLength >= tweetLength);
+}
+
+function handleCounter(stringLength) {
+  counter.textContent = tweetLength - stringLength;
+}
